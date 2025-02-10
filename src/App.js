f2d74b8bbdb40054d85f4653eafb6589e9e52ba7
mymac <trybook1901@gmail.com>
2025-02-10 22:42:33 +0900

    更新予定ラベル

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ function App() {
       <button onClick={handleAddTodo}>タスク追加</button>
       <button onClick={handleClear}>タスク削除</button>
       <div>残りのタスク: {todos.filter((todo) => !todo.completed).length}</div>
+      <div>更新予定のタスク</div>
     </>
   );
 }
