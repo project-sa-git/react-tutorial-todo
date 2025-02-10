6fe6cb86f5d917f57ffbb1ada8272e2137196703
mymac <trybook1901@gmail.com>
2025-02-10 22:55:32 +0900

    ヘッダー１追加

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ function App() {
       <h2>Todoリストサブ</h2>
       {/* TodoList: todosをまとめたコンポーネント */}
       <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <button>ヘッダー１</button>
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスク追加</button>
       <button>タスク更新（確定）</button>
